3d64f5a39afcf85a5a0a69c699d308e4904372bd
Kevinaie18 <aiekevin@icloud.com>
2024-10-21 10:52:13 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,5 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-require('dotenv').config();
+// Import user routes
+const userRoutes = require('./routes/users');
 
-app.use(express.json());
-
-// Import routes
-const missionRoutes = require('./routes/missions');
-
-// Use routes
-app.use('/api/missions', missionRoutes);
-
-const PORT = process.env.PORT || 3000;
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Use user routes
+app.use('/api/users', userRoutes);
